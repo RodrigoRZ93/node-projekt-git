b3a971024604cb5f93c11e733823c63f8186ce25
rodde93 <rodrigo.s-wdk@outlook.com>
2020-03-01 19:58:07 +0100

    commit insult.json & insult.db

--- seed.js
+++ seed.js
@@ -1,9 +1,10 @@
 
-const Datastore = require('nedb');
-const db = new Datastore({ filename: 'insults.db', autoload: true });
+const Datastore = require('nedb-promise');
+const insults = new Datastore({ filename: 'insults.db', autoload: true });
 let insultsList = require('./insults.json')
 
-db.remove({}).then(() => {
-    db.insert(insultList.insults);
+insults.remove({}).then(() => {
+    insults.insert(insultsList.insults);
 })
+
 
